08effb1e414021a9a42f5f0d4f0a6506e6e671eb
sikoramodra <wojciechmodro@gmail.com>
2025-04-19 12:09:31 +0200

    Init

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
+import * as pluginImportX from 'eslint-plugin-import-x';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default ts.config(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  // eslint-disable-next-line import-x/no-named-as-default-member
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
+  pluginImportX.flatConfigs.recommended,
+  pluginImportX.flatConfigs.typescript,
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: { 'simple-import-sort': simpleImportSort },
+    rules: { 'no-undef': 'off' },
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+    plugins: { 'simple-import-sort': simpleImportSort },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: ['.svelte'],
+        // eslint-disable-next-line import-x/no-named-as-default-member
+        parser: ts.parser,
+        svelteConfig,
+      },
+    },
+  },
+);
